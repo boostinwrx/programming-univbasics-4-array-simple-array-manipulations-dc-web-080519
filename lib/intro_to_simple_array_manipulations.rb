44f54d40e2efc5fbb46a854afd6324b2048c299c
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-07-22 23:09:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -45,6 +45,6 @@ def using_delete(instructors,string)
 end
 
 def using_delete_at(famous_robots, integer)
-  deleted_robot = famous_robots.delete(integer)
+  deleted_robot = famous_robots.delete_at(integer)
   return deleted_robot
 end
